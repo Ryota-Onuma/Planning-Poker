a4a80f709fe472defa8d43fc6ef0d24d64211e39
Ryota-Onuma <onumaryotajh.tech@gmail.com>
2023-03-01 01:00:49 +0900

    feat: ✨ update path

--- frontend/src/pages/user/signin/index.tsx
+++ frontend/src/pages/user/signin/index.tsx
@@ -3,14 +3,6 @@ import { useRouter } from "next/router";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const UserSigninPage = () => {
-  const router = useRouter();
-  const isReady = router.isReady;
-  const { isAuthenticated, isLoading } = useAuth0();
-  if (!isLoading && !isAuthenticated && isReady) {
-    router.push("/");
-    return;
-  }
-
   return <User />;
 };
 
