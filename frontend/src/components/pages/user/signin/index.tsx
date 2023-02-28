fdd38ba0f524d02ffc4fde8bec0e83e3b5a719a0
Ryota-Onuma <onumaryotajh.tech@gmail.com>
2023-03-01 01:35:31 +0900

    feat: ✨ fix

--- frontend/src/components/pages/user/signin/index.tsx
+++ frontend/src/components/pages/user/signin/index.tsx
@@ -14,15 +14,11 @@ const User = () => {
   useEffect(() => {
     if (typeof window !== "undefined" && isReady && isAuthenticated) {
       const redirect_url = localStorage.getItem('redirect_url')
-      console.log(redirect_url)
       if(redirect_url) {
         localStorage.removeItem('redirect_url');
-        console.log("if")
         router.push(redirect_url)
         return
       }else{
-        console.log("else")
-        console.log(redirect_url)
         router.push(`${process.env.NEXT_PUBLIC_ORIGIN}/room/choose`)
         return
       }
@@ -33,11 +29,9 @@ const User = () => {
     if (typeof window !== "undefined") {
       const searchParams = new URLSearchParams(window.location.search);
       if (searchParams.has("redirect_url")) {
-        localStorage.setItem('redirect_url', `${process.env.NEXT_PUBLIC_ORIGIN}/user/signin?redirect_url=${searchParams.get(
+        localStorage.setItem('redirect_url', `${process.env.NEXT_PUBLIC_ORIGIN}/room/play/${searchParams.get(
           "redirect_url"
         )}`);
-        const redirect_url = localStorage.getItem('redirect_url')
-        console.log(redirect_url)
         return `${process.env.NEXT_PUBLIC_ORIGIN}/user/signin`
       } else {
         return `${process.env.NEXT_PUBLIC_ORIGIN}/room/choose`
