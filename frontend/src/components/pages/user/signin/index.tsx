6b2deeb57ccca5117cb85059ac04d56e08e5a553
Ryota-Onuma <onumaryotajh.tech@gmail.com>
2023-02-28 23:11:52 +0900

    feat: ✨ update

--- frontend/src/components/pages/user/signin/index.tsx
+++ frontend/src/components/pages/user/signin/index.tsx
@@ -7,7 +7,7 @@ import { UserStyle as Style } from "./style";
 import router from "next/router";
 
 const User = () => {
-  const { loginWithPopup, isAuthenticated, isLoading, error } = useAuth0();
+  const { loginWithRedirect, isAuthenticated, isLoading, error } = useAuth0();
 
   const generateRedirectPath = () => {
     if (typeof window !== "undefined") {
@@ -35,7 +35,7 @@ const User = () => {
         {!isAuthenticated && (
           <Button
             onClick={() =>
-              loginWithPopup({
+              loginWithRedirect({
                 authorizationParams: {
                   redirect_uri: generateRedirectPath(),
                 },
